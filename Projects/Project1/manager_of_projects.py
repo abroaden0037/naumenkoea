97756c64a44ba6e47ee6e366b88d9ea42c2952ec
abroaden0037 <ballroom1309@mail.ru>
2025-09-30 12:22:40 +0300

    Only close() not ready

--- Projects/Project1/manager_of_projects.py
+++ Projects/Project1/manager_of_projects.py
@@ -1,3 +1,4 @@
+import os
 def amount_strings(file):
     f = open("/content/notes.txt",'r', encoding = "utf-8").readlines()
     return len(f)+1
@@ -6,18 +7,25 @@ def create():
     file = open("/content/notes.txt",'a+', encoding = "utf-8")
     file.write(f"{amount_strings(file)}. {input("Write your note:\n")}\n")
 def delete():
-    num = input("What note do you want to delete? Write the number of it.")
-    newfile = open("/content/notes.txt",'w', encoding = "utf-8")
+    deleted = input("What note do you want to delete? Write the number of it.\n")
+    newfile = open("/content/notes1.txt",'w', encoding = "utf-8")
     file = open("/content/notes.txt",'r', encoding = "utf-8")
-    i=1
+    num=1
     for i in file.readlines():
-      if a[0]!=num:
-        newfile.write(f"{1}. ",i[2:])
-    file = newfile
+        if i[0]!=deleted:
+          newfile.write(f"{num}. {i[3:]}")
+          num+=1
+    os.remove("/content/notes.txt")
+    os.rename("/content/notes1.txt","/content/notes.txt")
 def search():
-    pass
+    num = input("Which note do you want to search? Write the number of it.\n")
+    file = open("/content/notes.txt",'r', encoding = "utf-8").readlines()
+    for i in file:
+      if i[0] == num:
+        print(i)
+        break
 def close():
-    file.close()
+    pass
 def show():
     file = open("notes.txt",'r', encoding = "utf-8")
     print(file.read())
@@ -28,8 +36,8 @@ def interface():
             1. create a new note
             2. delete a note
             3. search a note
-            4. close a note
-            5. show all notes
+            4. close the programm
+            5. view all notes
         To select an action write the number of it.''')
       answer=input()
       match answer:
