50c285b87469a896dadfde80c98b17cf6b310c74
Elizaveta <ballroom1309@mail.ru>
2026-02-05 22:03:37 +0300

    Add files via upload

---
+++ Homeworks/Program.cs
@@ -0,0 +1,53 @@
+﻿interface IDamageable
+{
+    void TakeDamage(int damage);
+}
+
+abstract class Character: IDamageable
+{
+    private string Name;
+    private int Health;
+    protected Character()
+    {
+        this.Name = "Noname";
+        this.Health = 100;
+    }
+    protected Character(string name, int health)
+    {
+        this.Name = name;
+        this.Health = health;
+    }
+    public abstract void Attact();
+    public void Move()
+    {
+        Console.WriteLine("Character moved");
+    }
+    public void TakeDamage(int Damage)
+    {
+        this.Health -= 1;
+    }
+}
+// абстрактный тк есть абстрактный метод
+class Warrior: Character
+{
+    public override void Attact()
+    {
+        Console.WriteLine("Warior attacked");
+    }
+}
+class Mage: Character
+{
+    public override void Attact()
+    {
+        Console.WriteLine("Mage attacked");
+    }
+}
+//потому что методы оверрайд
+
+class Ward : Character
+{
+    public override void Attact()
+    {
+        Console.WriteLine("Ward attacked");
+    }
+}
